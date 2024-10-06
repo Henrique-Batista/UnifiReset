abe3a0cd8dd5ab4d1aa215e56e48ac29ba656569
Pablo Henrique Batista <siriatosdravate@gmail.com>
2024-10-05 22:03:36 -0300

    Removido senha anterior, organizado o codigo em funcoes e melhorado interface

--- UnifiReset/Program.cs
+++ UnifiReset/Program.cs
@@ -2,45 +2,107 @@
 using MongoDB.Driver;
 using System.Diagnostics;
 
-// Inicializa o acesso ao banco de dados
-var client = new MongoClient("mongodb://localhost:27117");
-var database = client.GetDatabase("ace");
-var collection = database.GetCollection<BsonDocument>("admin");
-var filter = Builders<BsonDocument>.Filter.Empty;
-var document = collection.Find(filter).FirstOrDefault();
-var x_shadow = document.GetValue("x_shadow");
-var id = document.GetValue("_id");
-var username = document.GetValue("name");
+string? enderecoBanco = null;
+string? portaBanco = null;
+MongoClient client = null;
+IMongoDatabase database = null;
+IMongoCollection<BsonDocument> collection = null;
+FilterDefinition<BsonDocument> filter = null;
+BsonDocument? document = null;
+BsonValue? x_shadow = null;
+BsonValue? id = null;
+BsonValue? username = null;
+string senha = null;
+string senhaCriptografada = null;
 
-// Cria interface do usuario
+
 Console.WriteLine("Este programa realiza o acesso ao banco de dados local do Unifi Controller e altera o hash da senha armazenada para o primeiro usuario admin.\n");
-Console.WriteLine("Digite a nova senha");
-var senha = Console.ReadLine();
-Console.WriteLine();
 
-// Executa o binario do mkpasswd e armazena a senha gerada
-var process = new Process();
-var startinfo = new ProcessStartInfo();
-byte[] binaryData = UnifiReset.Properties.Resources.mkpasswd;
-string tempFilePath = Path.Combine(Path.GetTempPath(), "mkpasswd.exe");
-File.WriteAllBytes(tempFilePath, binaryData);
-startinfo.FileName = tempFilePath;
-startinfo.UseShellExecute = false;
-startinfo.Arguments = $"-hash sha512 -salt \"9Ter1EZ9$lSt6\" -password \"{senha}\"";
-startinfo.RedirectStandardOutput = true;
-process.StartInfo = startinfo;
-process.Start();
-string senhaCriptografada = process.StandardOutput.ReadToEnd().Replace("\n", "").Replace("\r", "");
-
-Console.WriteLine($"Senha criptografada: {senhaCriptografada}\n");
-Console.WriteLine("Nome do usuario:" + username + "\nId do usuario:" + id + "\n" + "Senha anterior:" + x_shadow + "\n");
-
-// Atualiza o banco de dados com a informacao da senha
-var update = Builders<BsonDocument>.Update.Set("x_shadow", senhaCriptografada);
-collection.UpdateOne(filter,update);
-
-Console.WriteLine("Programa finalizado");
-Console.ReadLine();
+Console.WriteLine("\nInforme o endereco do banco ( Deixe vazio para utilizar o padrao localhost )");
+enderecoBanco = Console.ReadLine();
+Console.WriteLine("Informe a porta do banco ( Deixe vazio para utilizar o padrao 27117 )");
+portaBanco = Console.ReadLine();
 
 
+Console.Write("Tentando conectar no banco...");
+try
+{
 
+    ConectaBanco(enderecoBanco, portaBanco);
+    GeraInterface();
+    if (senha != String.Empty)
+    {
+        ExecutaMkPasswd();
+        ExibeInfo();
+    }
+}
+catch (TimeoutException)
+{
+    Console.WriteLine(
+        "\nHouve um problema para se conectar ao banco, por favor verifique se ele esta online, se o endereco esta correto e se esta funcionando na porta informada.");
+}
+catch (Exception)
+{
+    Console.WriteLine("\nHouve algum problema na aplicacao, por favor contate o desenvolvedor.");
+}
+
+void GeraInterface(){
+    Console.Write("OK");
+    Console.WriteLine("\nDigite a nova senha");
+    senha = Console.ReadLine();
+    Console.WriteLine();
+}
+void ConectaBanco(string? endereco, string? porta)
+{
+    if (endereco == String.Empty)
+    {
+        endereco = "localhost";
+        Console.Write("Endereco invalido, utilizando endereco padrao localhost...");
+    }
+    if ((porta == String.Empty) || (Int32.TryParse(porta, out int portaParseado) == false))
+    {
+        porta = Convert.ToString(27117);
+        Console.Write("Porta invalida, utilizando porta padrao 27117...");
+    }
+    else
+    {
+        porta = Convert.ToString(porta);
+    }
+
+    client = new MongoClient($"mongodb://{endereco}:{porta}");
+    database = client.GetDatabase("ace");
+    collection = database.GetCollection<BsonDocument>("admin");
+    filter = Builders<BsonDocument>.Filter.Empty;
+    document = collection.Find(filter).FirstOrDefault();
+    x_shadow = document.GetValue("x_shadow");
+    id = document.GetValue("_id");
+    username = document.GetValue("name");
+}
+
+void ExecutaMkPasswd()
+{
+    var process = new Process();
+    var startinfo = new ProcessStartInfo();
+    byte[] binaryData = UnifiReset.Properties.Resources.mkpasswd;
+    string tempFilePath = Path.Combine(Path.GetTempPath(), "mkpasswd.exe");
+    File.WriteAllBytes(tempFilePath, binaryData);
+    startinfo.FileName = tempFilePath;
+    startinfo.UseShellExecute = false;
+    startinfo.Arguments = $"-hash sha512 -salt \"9Ter1EZ9$lSt6\" -password \"{senha}\"";
+    startinfo.RedirectStandardOutput = true;
+    process.StartInfo = startinfo;
+    process.Start();
+    senhaCriptografada = process.StandardOutput.ReadToEnd().Replace("\n", "").Replace("\r", "");
+}
+
+void ExibeInfo()
+{
+    Console.WriteLine($"\nSenha criptografada: {senhaCriptografada}\n");
+    Console.WriteLine("Nome do usuario:" + username + "\nId do usuario:" + id + "\n");
+    // Atualiza o banco de dados com a informacao da senha
+    var update = Builders<BsonDocument>.Update.Set("x_shadow", senhaCriptografada);
+    collection.UpdateOne(filter, update);
+}
+
+Console.WriteLine("\nPrograma finalizado, digite qualquer tecla para encerrar.");
+Console.ReadKey();
